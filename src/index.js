b6115edb85870999694927a3b367096781b299f5
isacvale <isac@ocastudios.com>
2021-08-22 11:05:44 -0700

    Adjustment to tests and coverage.

--- src/index.js
+++ src/index.js
@@ -1,109 +1,109 @@
-const config = this ? this.config : {}
-let loadedTags = []
+const config = this ? this.config : {};
+let loadedTags = [];
 
 const makeContainer = (target) => {
-  const newContainer = document.createElement('div')
-  newContainer.id = target
-  return document.body.appendChild(newContainer)
-}
+  const newContainer = document.createElement("div");
+  newContainer.id = target;
+  return document.body.appendChild(newContainer);
+};
 
 const isNewCustomTag = (tagName) => {
-  if (!tagName.includes('-')) return false
-  if (loadedTags.includes(tagName)) return false
-  return true
-}
+  if (!tagName.includes("-")) return false;
+  if (loadedTags.includes(tagName)) return false;
+  return true;
+};
 
 const fetchMarkUp = async (tagName, path) => {
   try {
-    const content = fetch(`${path}/${tagName}.html`)
-    return (await content).text()
+    const content = fetch(`${path}/${tagName}.html`);
+    return (await content).text();
   } catch (error) {
-    console.warn(`Component <${tagName}> could not be fetched: ${error}`)
+    console.warn(`Component <${tagName}> could not be fetched: ${error}`);
   }
-}
+};
 
 const injectMarkUp = (markUp) => {
-  if (!markUp) return false
-  const markUpFragment = document.createDocumentFragment()
-  const container = document.createElement('div')
-  container.innerHTML = markUp
-  while (container.firstChild) markUpFragment.appendChild(container.firstChild)
-  return markUpFragment
-}
+  if (!markUp) return false;
+  const markUpFragment = document.createDocumentFragment();
+  const container = document.createElement("div");
+  container.innerHTML = markUp;
+  while (container.firstChild) markUpFragment.appendChild(container.firstChild);
+  return markUpFragment;
+};
 
 const activateScripts = (fragment) => {
-  if (!fragment) return null
-  fragment.querySelectorAll('script').forEach((script) => {
-    const newScriptElement = document.createElement('script')
-    if (script.getAttribute('type') === 'module') {
-      newScriptElement.setAttribute('type', 'module')
+  if (!fragment) return null;
+  fragment.querySelectorAll("script").forEach((script) => {
+    const newScriptElement = document.createElement("script");
+    if (script.getAttribute("type") === "module") {
+      newScriptElement.setAttribute("type", "module");
     }
-    newScriptElement.appendChild(document.createTextNode(script.innerHTML))
-    const container = script.parentElement || fragment
-    container.appendChild(newScriptElement)
-    container.removeChild(script)
-  })
-}
+    newScriptElement.appendChild(document.createTextNode(script.innerHTML));
+    const container = script.parentElement || fragment;
+    container.appendChild(newScriptElement);
+    container.removeChild(script);
+  });
+};
 
 const parseElement = async (tagName, el, path, container) => {
-  loadedTags.push(tagName)
-  const markUp = await fetchMarkUp(tagName, path)
-  const fragment = injectMarkUp(markUp)
-  activateScripts(fragment)
-  fragment && container.append(fragment)
-  el.shadowRoot && monitorElements(el.shadowRoot.childNodes)
-}
+  loadedTags.push(tagName);
+  const markUp = await fetchMarkUp(tagName, path);
+  const fragment = injectMarkUp(markUp);
+  activateScripts(fragment);
+  fragment && container.append(fragment);
+  el.shadowRoot && monitorElements(el.shadowRoot.childNodes);
+};
 
 const getPath = (tagName) => {
-  const pathsDict = (config && config.chips && config.chips.paths) || {}
-  const pathDefault = config && config.chips && config.chips.path
-  return pathsDict[tagName] || pathDefault || '/components'
-}
+  const pathsDict = (config && config.chips && config.chips.paths) || {};
+  const pathDefault = config && config.chips && config.chips.path;
+  return pathsDict[tagName] || pathDefault || "/components";
+};
 
 const monitorElements = (container) => (mutationList) => {
   mutationList.forEach(async (mutation) => {
-    if (mutation.type !== 'childList') return
+    if (mutation.type !== "childList") return;
 
-    const el = mutation.addedNodes ? mutation.addedNodes[0] : mutation
-    if (!(el instanceof HTMLElement)) return
+    const el = mutation.addedNodes ? mutation.addedNodes[0] : mutation;
+    if (!(el instanceof HTMLElement)) return;
 
-    monitorElements(el.childNodes)
+    monitorElements(el.childNodes);
 
-    const tagName = el.tagName.toLowerCase()
-    if (!isNewCustomTag(tagName)) return
+    const tagName = el.tagName.toLowerCase();
+    if (!isNewCustomTag(tagName)) return;
 
-    const path = getPath(tagName)
+    const path = getPath(tagName);
 
     const intersectionObserver = new IntersectionObserver(
       (entries, observer) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
-            observer.unobserve(el)
-            parseElement(tagName, el, path, container)
+            observer.unobserve(el);
+            parseElement(tagName, el, path, container);
           }
-        })
+        });
       },
       {
-        rootMargin: '10%'
+        rootMargin: "10%",
       }
-    )
-    intersectionObserver.observe(el)
-  })
-}
+    );
+    intersectionObserver.observe(el);
+  });
+};
 
-const chips = ({ target = '_chips' } = {}) => {
-  const container = makeContainer(target)
+const chips = ({ target = "_chips" } = {}) => {
+  const container = makeContainer(target);
 
-  const mutationObserver = new MutationObserver(monitorElements(container))
-  mutationObserver.observe(document, { childList: true, subtree: true })
+  const mutationObserver = new MutationObserver(monitorElements(container));
+  mutationObserver.observe(document, { childList: true, subtree: true });
 
-  return { loadedTags }
-}
+  return { loadedTags };
+};
 
-chips(config && config.chips)
+chips(config && config.chips);
 
 // For testing purposes
-
+/*
 const reset = ({ config: configArg, loadedTags: loadedTagsArg = [] } = {}) => {
   // config = {}
   loadedTags = loadedTagsArg
@@ -121,3 +121,4 @@ export {
   monitorElements,
   chips
 }
+*/
